c432211353dc328344eb05092d64f9b1b70b6db2
Mahmoud Hosni <mah1998mod@gmail.com>
2023-01-06 10:15:30 +0200

    fix dropdown button on nav component at home page

--- Frontend/src/app/components/header/menu/menu.component.ts
+++ Frontend/src/app/components/header/menu/menu.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/Interfaces/Category';
+import { CategoryService } from 'src/app/_services/category.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
 })
 export class MenuComponent implements OnInit {
-  
-  constructor() { }
+  constructor(private _categoryService: CategoryService) {}
 
   ngOnInit(): void {
+    this._categoryService.getCategories();
   }
 
+  get categories(): Category[] {
+    return this._categoryService.categories;
+  }
 }
